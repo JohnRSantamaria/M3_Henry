a23ea7c7e99fc2e3d43c7ba2abac4e145767ba50
JohnRSantamaria <santamariajohnd@gmail.com>
2023-02-08 19:51:40 -0500

    AdvancePromises spec 1 ended

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -3,8 +3,32 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor) {
+  if (typeof executor !== "function") throw TypeError("executor must be a function");
 
+  this._state = "pending";
+  this._value = undefined;
 
+  const resolve = (data) => this._internalResolve(data);
+  const reject = (reason) => this._internalReject(reason);
+  
+  executor(resolve, reject);
+
+}
+
+$Promise.prototype._internalResolve = function (data) {
+  if (this._state === "pending") {
+    this._value = data;
+    this._state = "fulfilled";
+  }
+};
+
+$Promise.prototype._internalReject = function (reason) {
+  if (this._state === "pending") {
+    this._value = reason;
+    this._state = "rejected";
+  }
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
