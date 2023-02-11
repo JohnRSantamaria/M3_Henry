e7f602f06cf8e93fc45cc1f5004bd24050f37627
JohnRSantamaria <santamariajohnd@gmail.com>
2023-02-11 18:27:02 -0500

    chapter 2 ended

--- 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ 04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -4,14 +4,28 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 function $Promise(executor) {
-  if (typeof executor !== "function") throw TypeError("executor must be a function");
+  if (typeof executor !== "function")
+    throw TypeError("executor must be a function");
 
   this._state = "pending";
   this._value = undefined;
+  this._handlerGroups = [];
+
+  this.callHandlers = (value) => {
+    while (this._handlerGroups.length > 0) {
+      const currentHandler = this._handlerGroups.shift();
+      this._state === "fulfilled" &&
+        currentHandler.successCb &&
+        currentHandler.successCb(value);
+      this._state === "rejected" &&
+        currentHandler.errorCb &&
+        currentHandler.errorCb(value);
+    }
+  };
 
   const resolve = (data) => this._internalResolve(data);
   const reject = (reason) => this._internalReject(reason);
-  
+
   executor(resolve, reject);
 
 }
@@ -20,6 +34,7 @@ $Promise.prototype._internalResolve = function (data) {
   if (this._state === "pending") {
     this._value = data;
     this._state = "fulfilled";
+    this.callHandlers(this._value);
   }
 };
 
@@ -27,9 +42,19 @@ $Promise.prototype._internalReject = function (reason) {
   if (this._state === "pending") {
     this._value = reason;
     this._state = "rejected";
+    this.callHandlers(this._value);
   }
 };
 
+$Promise.prototype.then = function (successHandler, succressError) {
+  const handerGroup = {
+    successCb: typeof successHandler === "function" ? successHandler : false,
+    errorCb: typeof succressError === "function" ? succressError : false,
+  };
+
+  this._handlerGroups.push(handerGroup);
+  this._state !== 'pending' && this.callHandlers(this._value  );
+};
 module.exports = $Promise;
 /*-------------------------------------------------------
 El spec fue diseñado para funcionar con Test'Em, por lo tanto no necesitamos
